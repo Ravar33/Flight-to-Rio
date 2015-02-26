4e1a9c109d65950402cc463ad2d2a813530dd749
Ruben Wouters <rubenwouters404@hotmail.com>
2015-02-26 12:12:30 +0100

    Upgrade images added

--- flight_to_rio/www/js/index.js
+++ flight_to_rio/www/js/index.js
@@ -27,27 +27,30 @@ clickedObject.addEventListener("click", function(evt)
 {
     var upgradeTitle = document.getElementById("upgradeTitle");
     var upgradeExplanation = document.getElementById("upgradeExplanation");
-    var description;
+    var description, imgSrc;
 
     upgradeTitle.innerHTML = evt.target.alt;
     openDescription.style.visibility = "visible";
 
-    var explanation;
+    var explanation, imgSrc;
     switch(evt.target.alt)
     {
        case "GunPowder":
+            imgSrc = "img/Price_Gunpowder.png";
             if(gunpowderLevel < 4 && parseInt(amountOfMoney) > 10000){
                 explanation = "Gun Powder upgraded to level: " + gunpowderLevel;
             }
             else{explanation = "You have achieved the maximum level of 4 or you do not have 10000 money"}
             break;
         case "PowerBar":
+            imgSrc = "img/Price_PowerBar.png";
             if(powerbarLevel < 4 && parseInt(amountOfMoney) > 30000){
                 explanation = "PowerBar upgraded to level: " + powerbarLevel;
             }
             else{explanation = "You have achieved the maximum level of 4 or you do not have 30000 money"}
             break;
         case "RedBull":
+            imgSrc = "img/Price_RedBull.png";
             if(redBullLevel < 4 && parseInt(amountOfMoney) > 20000){
                 explanation = "RedBull upgraded to level: " + redBullLevel;
             }
@@ -59,6 +62,7 @@ clickedObject.addEventListener("click", function(evt)
     }
 
     upgradeExplanation.innerHTML = explanation;
+    document.getElementById("priceImg").src = imgSrc;
 });
 
 closeDescription.addEventListener("click", function()
