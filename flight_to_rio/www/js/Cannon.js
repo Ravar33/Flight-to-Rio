10a16632b1982d45ac6036f008a7942c79010066
DriesVS <van.schevensteen.dries@gmail.com>
2015-02-26 16:58:37 +0100

    Added cannon graphics

--- flight_to_rio/www/js/Cannon.js
+++ flight_to_rio/www/js/Cannon.js
@@ -8,7 +8,7 @@
 
 (function() {
 
-	var shooter, shooterHandle;
+	var shooter, shooterHandle, shooterBitmap;
 
 	function Cannon(stageWidth, stageHeight, xOffset, yBottomOffset) {
 
@@ -30,8 +30,12 @@
 
 	p.setup = function() {
 
-		var base = new createjs.Shape();
-		base.graphics.beginFill("blue").drawRect(this.consts.xOffset, this.consts.stageHeight - this.consts.yBottomOffset, 30, -30);
+		var bitmapBase = new createjs.Bitmap("img/cannon_wheel.png");
+		bitmapBase.regX = 0;
+		bitmapBase.regY = 0;
+		bitmapBase.x = this.consts.xOffset;
+		bitmapBase.y = this.consts.stageHeight - this.consts.yBottomOffset - 44;
+		bitmapBase.scaleX = bitmapBase.scaleY = 1;
 
 		shooter = new createjs.Shape();
 		shooter.graphics.beginFill("blue").drawRect(0, -10, 90, 20);
@@ -40,20 +44,36 @@
 		shooter.regY = 0;
 		shooter.x = this.consts.xOffset + 20;
 		shooter.y = this.consts.stageHeight - this.consts.yBottomOffset - 20;
+		
+		shooterBitmap = new createjs.Bitmap("img/cannon_body.png");
+		shooterBitmap.regX = 32;
+		shooterBitmap.regY = 164;
+		shooterBitmap.rotation = 45;
+		shooterBitmap.x = this.consts.xOffset + 20;
+		shooterBitmap.y = this.consts.stageHeight - this.consts.yBottomOffset - 20;
+		shooterBitmap.scaleX = shooterBitmap.scaleY = .5;
 
 		shooterHandle = {
 			"x": shooter.x,
 			"y": shooter.y
 		}
 
-		this.addChild(base, shooter);
+		this.addChild(bitmapBase, shooterBitmap);
 	};
 
 	p.rotateShooter = function(angle, min, max) {
 
-		if (angle >= min && angle <= max) shooter.rotation = angle;
-		else if (angle > max) shooter.rotation = max;
-		else shooter.rotation = min;
+		if (angle >= min && angle <= max) {
+			shooter.rotation = angle;
+			shooterBitmap.rotation = angle + 90;
+			
+		} else if (angle > max) {
+			shooter.rotation = max;
+			shooterBitmap.rotation = max;
+		} else {
+			shooter.rotation = min;
+			shooterBitmap.rotation = min;
+		}
 	};
 
 	/** Caculates the angle from an input point with the base point of the cannon **/
